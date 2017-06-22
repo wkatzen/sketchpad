9001d4d33d3e0ba598a098f59b581a26273ccda7
Wolcott Katzenbach <wkatzen@gmail.com>
2017-06-21 19:30:40 -0700

    Created index.html, stylesheet, javascript and README files. First attempts at creating 16x16 div grid in javascript.

---
+++ README.md
@@ -0,0 +1,2 @@
+From The Odin Project Javascript and jQuery project:
+https://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery?ref=lnav
---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+function createGrid() {
+	$("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+  $("#container").append('<div><p>working</p></div>');
+}
+$(document).ready(function() {
+  createGrid();
+});
