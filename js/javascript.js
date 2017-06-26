a6ed9d588819f9bcc6ecf45f209c2a3875dca074
Wolcott Katzenbach <wkatzen@gmail.com>
2017-06-26 11:49:58 -0700

    HTML: Added button for upcoming new grid function. CSS: Added initial styling for button (which looks to be redundant) and styling for sketchpad grid and .active class. JS: Made createGrid function, active mouse function and initial new grid prompt.

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,22 @@
-function createGrid() {
-	$("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-  $("#container").append('<div><p>working</p></div>');
-}
+var size = 16;
+var gridSize = 20;
+
 $(document).ready(function() {
-  createGrid();
+  	createGrid()
+    $('button').click(function() {
+      $('div').removeClass("active");
+      prompt("Pick a new grid size:");
+    })
+// makes initial grid
+   function createGrid() {
+   	 for(i = 1; i <= size * size; i++) {
+ 			 $("#container").append('<div class="square"></div>');
+   };
+ };
+
+ // when mouse enters the grid div changes class to show new color
+    $(document).on("mouseenter", ".square", function() {
+      $(this).addClass("active");
+    });
+
 });
